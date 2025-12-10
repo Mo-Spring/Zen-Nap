b7bbd21465a0e1075d62b07a4d64a671c98af1a9
Mo-Spring <110386992+Mo-Spring@users.noreply.github.com>
2025-12-10 23:02:51 +0800

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,8 @@
+// src/constants.ts
+export const MODES = [
+  { id: 'deep', name: '深度睡眠', iconType: 'Moon', durationMinutes: 30, bgImage: 'https://picsum.photos/id/1/1000/1000', themeColor: '#1a1a2e' },
+  { id: 'power', name: '快速充电', iconType: 'Lightning', durationMinutes: 15, bgImage: 'https://picsum.photos/id/2/1000/1000', themeColor: '#0f172a' },
+  { id: 'focus', name: '专注模式', iconType: 'Focus', durationMinutes: 25, bgImage: 'https://picsum.photos/id/3/1000/1000', themeColor: '#0c1b3d' },
+  { id: 'relax', name: '放松呼吸', iconType: 'Breath', durationMinutes: 10, bgImage: 'https://picsum.photos/id/4/1000/1000', themeColor: '#0a2447' },
+  { id: 'custom', name: '自定义', iconType: 'Custom', durationMinutes: 30, bgImage: 'https://picsum.photos/id/5/1000/1000', themeColor: '#1e40af' }
+];
