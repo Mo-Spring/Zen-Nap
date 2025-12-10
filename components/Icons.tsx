4c3cc162ffbbb5e2c15f76d47a4299970f36e59a
Mo-Spring <110386992+Mo-Spring@users.noreply.github.com>
2025-12-10 16:32:06 +0800

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,6 +1,5 @@
 
-import React from 'react';
-import { Zap, Coffee, Plane, Moon, Settings, Music, BarChart2, X, ChevronLeft, Info, Hexagon } from 'lucide-react';
+import { Zap, Coffee, Plane, Moon, Settings, BarChart2, ChevronLeft, Info, Hexagon } from 'lucide-react';
 
 export const IconMap = {
   lightning: Zap,
