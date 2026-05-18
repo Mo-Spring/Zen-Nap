97d1c72a1210054ec75deaba5256bac7a3500e04
Mo-Spring <110386992+Mo-Spring@users.noreply.github.com>
2026-05-18 16:49:42 +0800

    Update index.ts

--- plugins/alarm-channel/index.ts
+++ plugins/alarm-channel/index.ts
@@ -4,6 +4,8 @@ export interface AlarmChannelPlugin {
     createAlarmChannel(options: { channelId?: string; channelName?: string }): Promise<{ success: boolean; message?: string }>;
     playAlarmAudio(options: { uri: string; loop?: boolean }): Promise<{ success: boolean }>;
     stopAlarmAudio(): Promise<{ success: boolean }>;
+    scheduleAlarm(options: { uri?: string; triggerAtMillis: number; loop?: boolean }): Promise<{ success: boolean }>;
+    cancelScheduledAlarm(): Promise<{ success: boolean }>;
 }
 
 const AlarmChannel = registerPlugin<AlarmChannelPlugin>('AlarmChannel');
