aad91500225a4c04600c890264441c809381f008
Mo-Spring <110386992+Mo-Spring@users.noreply.github.com>
2026-05-18 11:48:14 +0800

    Update index.ts

--- plugins/alarm-channel/index.ts
+++ plugins/alarm-channel/index.ts
@@ -2,6 +2,8 @@ import { registerPlugin } from '@capacitor/core';
 
 export interface AlarmChannelPlugin {
     createAlarmChannel(options: { channelId?: string; channelName?: string }): Promise<{ success: boolean; message?: string }>;
+    playAlarmAudio(options: { uri: string; loop?: boolean }): Promise<{ success: boolean }>;
+    stopAlarmAudio(): Promise<{ success: boolean }>;
 }
 
 const AlarmChannel = registerPlugin<AlarmChannelPlugin>('AlarmChannel');
